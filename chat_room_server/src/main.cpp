a891d1a320a25f240a302ff51ec2d166399f0031
davidzdravkovic <davidzdravkovic21@gmail.com>
2026-07-28 17:44:37 +0200

    Clean up main.cpp: remove unused includes and draft comments

--- chat_room_server/src/main.cpp
+++ chat_room_server/src/main.cpp
@@ -1,29 +1,7 @@
-#include <iostream>
-#include <boost/asio.hpp>
-#include <boost/system.hpp>
-#include <pqxx/pqxx>
 #include "../include/Server/Server.h"
-
-
-
-
 
 int main()
 {
-  //Later with compositor root this object will be created !
     Server server;
     server.run();
-    // Keep running; Network::run() blocks on io_context under systemd.
-
 }
-// operativen sistem ima zadaca da prifati korisnik
-// ako e zavrsena funckijata USPESNO od OS izvrsi - > funkcionalnostite vo lambdata, ALI sekako koga kje zavrsi OS operacijata povikaj nov accept 
-//  operativen sistem ima zadaca da prifati korisnik
-// ako e zavrsena funckijata USPESNO od OS izvrsi - > funkcionalnostite vo lambdata, ALI sekako koga kje zavrsi OS operacijata povikaj nov accept 
-//operativen sistem ima zadaca da prifati korisnik
-// ako e zavrsena funckijata USPESNO od OS izvrsi - > funkcionalnostite vo lambdata, ALI sekako koga kje zavrsi OS operacijata povikaj nov accept 
-//operativen sistem ima zadaca da prifati korisnik
-// ako e zavrsena funckijata USPESNO od OS izvrsi - > funkcionalnostite vo lambdata, ALI sekako koga kje zavrsi OS operacijata povikaj nov accept 
-///operativen sistem ima zadaca da prifati korisnik
-// ako e zavrsena funckijata USPESNO od OS izvrsi - > funkcionalnostite vo lambdata, ALI sekako koga kje zavrsi OS operacijata povikaj nov accept 
-//
